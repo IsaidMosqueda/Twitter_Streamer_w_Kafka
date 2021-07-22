ca020cc56ef8c4637e00efde82991d7fef9db76f
Raúl Isaid Mosqueda <71615610+IsaidMosqueda@users.noreply.github.com>
2021-07-21 21:47:35 -0500

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,9 @@
+# Variables that contains the user credentials to access Twitter API 
+
+
+KEY = '________'
+SECRET = '________'
+Bearer_Token = '________'
+
+ACCESS_TOKEN = '________'
+ACCESS_TOKEN_SECRET = '________'
